44de3ebbdea56ea584f76510480860f2bcd50677
Gary1924 <gary_1924@qq.com>
2016-07-05 03:00:20 +0800

    test
    just test

---
+++ test.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @authors Gary_zhou (gary_1924@qq.com)
+ * @date    2016-07-05 02:32:11
+ * @version v1.0.0
+ */
+
+ function test() {
+ 	console.log('test');
+ }
+
+exports.test = test;
